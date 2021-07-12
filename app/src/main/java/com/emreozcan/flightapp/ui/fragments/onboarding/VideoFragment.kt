b9deaa0595cd6a926df2f67e0a05ff47d35d83df
Emre Ozcan <eozcan97@gmail.com>
2021-07-12 17:49:42 +0300

    Youtube Player Changed

--- app/src/main/java/com/emreozcan/flightapp/ui/fragments/onboarding/VideoFragment.kt
+++ app/src/main/java/com/emreozcan/flightapp/ui/fragments/onboarding/VideoFragment.kt
@@ -3,28 +3,20 @@ package com.emreozcan.flightapp.ui.fragments.onboarding
 import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil.setContentView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.emreozcan.flightapp.R
 import com.emreozcan.flightapp.databinding.FragmentVideoBinding
-import com.emreozcan.flightapp.util.Constants.Companion.YOUTUBE_PLAYER_KEY
-import com.emreozcan.flightapp.util.Constants.Companion.YOUTUBE_VIDEO_KEY
 import com.emreozcan.flightapp.viewmodel.MainViewModel
-import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
-import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener
-import com.google.android.youtube.player.YouTubePlayer.Provider
-import com.google.android.youtube.player.YouTubePlayerSupportFragmentXKt
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerFullScreenListener
 
 
 class VideoFragment : Fragment() {
@@ -35,8 +27,6 @@ class VideoFragment : Fragment() {
     private val mainViewModel: MainViewModel by viewModels()
 
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -45,40 +35,29 @@ class VideoFragment : Fragment() {
         _binding = FragmentVideoBinding.inflate(inflater, container, false)
         val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
 
-        binding.buttonContinueVideo.setOnClickListener {
+        binding.buttonContinueVideo?.setOnClickListener {
             viewPager?.currentItem =  1
         }
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+            binding.youtubePlayerView.enterFullScreen()
+        }
 
-        val youTubePlayerFragment = YouTubePlayerSupportFragmentXKt.newInstance()
 
-        val transaction = childFragmentManager.beginTransaction()
-        transaction.add(R.id.youtubeLayout, youTubePlayerFragment).commit()
-
-        youTubePlayerFragment.initialize(YOUTUBE_PLAYER_KEY, object : OnInitializedListener {
-            override fun onInitializationSuccess(p0: Provider?, p1: YouTubePlayer?, p2: Boolean) {
-                p1?.loadVideo(YOUTUBE_VIDEO_KEY)
-                var check = false
-                p1?.setOnFullscreenListener {
-                    if (check) {
-                        activity?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-                        check = false
-                    } else {
-
-                        check = true
-                    }
+        binding.youtubePlayerView.addFullScreenListener(object : YouTubePlayerFullScreenListener {
+            override fun onYouTubePlayerEnterFullScreen() {
+                val orientation = resources.configuration.orientation
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+                    activity?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
                 }
-                p1?.play()
+                activity?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
             }
 
-            override fun onInitializationFailure(p0: Provider?, p1: YouTubeInitializationResult?) {
-                val errorMessage = p1.toString()
-                Toast.makeText(activity, errorMessage, Toast.LENGTH_LONG).show()
-                Log.d("errorMessage:", errorMessage)
+            override fun onYouTubePlayerExitFullScreen() {
             }
-
         })
 
-        binding.firstScreenFinish.setOnClickListener {
+        binding.firstScreenFinish?.setOnClickListener {
             mainViewModel.saveOnboarding()
             val orientation = getResources().getConfiguration().orientation
             if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
